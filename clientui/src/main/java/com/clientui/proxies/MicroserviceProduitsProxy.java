906fa022ea2d8d258e5af9bac53db4bb60ff2487
jeromebodart <jerome.bodart@hei.yncrea.fr>
2019-05-16 17:24:38 +0200

    Part_2

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -1,13 +1,13 @@
 package com.clientui.proxies;
 
-import com.clientui.beans.ProductBean;
+import java.util.List;
+
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-import java.util.Optional;
+import com.clientui.beans.ProductBean;
 
 @FeignClient(name = "zuul-server")
 @RibbonClient(name = "microservice-produits")
--- microservice-expedition/src/main/java/com/mexpedition/MicroserviceExpeditionApplication.java
+++ microservice-expedition/src/main/java/com/mexpedition/MicroserviceExpeditionApplication.java
@@ -3,10 +3,11 @@ package com.mexpedition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class MicroserviceExpeditionApplication {
 
 	public static void main(String[] args) {
